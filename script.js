913d7f894130f175586ad590325ef2f5c8f578b5
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-28 15:11:42 +0600

    added new elements

--- script.js
+++ script.js
@@ -4,8 +4,37 @@
 //    console.log(item)
 // });
 
-let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "Petter", lastname: "Parker"}, 12345]
+// let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "Petter", lastname: "Parker"}, 12345]
 
-fruits.forEach((item=> {
-   console.log(typeof(item));
-}))
+// fruits.forEach((item=> {
+//    console.log(typeof(item));
+// }))
+
+
+let people = [
+   {
+      name: "Peter",
+      lastname: "Parker",
+      age: 34
+   },
+   {
+      name: "Lasley",
+      lastname:"Johnson",
+      age: 55
+   },
+   {
+      name: "Sam",
+      lastname:"Cruz",
+      age: 11
+   },
+   {
+      name: "Sadyr",
+      lastname:"Japarov",
+      age: 44
+   },
+   {
+      name: "Elon",
+      lastname:"Mask",
+      age: 51
+   },
+]
