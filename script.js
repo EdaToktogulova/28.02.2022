6241e67941e0af4848714e741143a30b6a674239
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-28 15:12:32 +0600

    added new element

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 //    console.log(item)
 // });
 
-// let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "Petter", lastname: "Parker"}, 12345]
+let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "Petter", lastname: "Parker"}, 12345]
 
 // fruits.forEach((item=> {
 //    console.log(typeof(item));
