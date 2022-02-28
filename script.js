7bad9d454baac8632a4c382755b91bcf03ee963b
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-28 14:42:54 +0600

    added let fruits[...]

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let fruits = ["apple", "banana", "qiwi", "peach"]
-array.forEach(element => {
-   
-});
+let fruits = ["apple", "banana", "qiwi", "peach",]
+
+// fruits.forEach((item) => {
+//    console.log(item)
+// });
+
+let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "Petter", lastname: "Parker"}, 12345]
