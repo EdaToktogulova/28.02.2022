2e7cbf013a30e15e00ed90d686449bcf7d0c3a33
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-03-02 15:16:17 +0600

    added new class accordions

--- script.js
+++ script.js
@@ -45,7 +45,16 @@
 // });
 
 document.querySelectorAll('.accordion').forEach(accordion => {
+   const parent = accordion.closest('.accordion');
+
    accordion.querySelector('.accordion__header').addEventListener('click', function() {
+      if (parent && parent.classList.contains('accordion--toggle')) {
+         parent.querySelectorAll('accordion').forEach(accordion => {
+            accordion.classList.remove("accordion--open");
+         });
+      }
+
+
       accordion.classList.toggle('accordion--open');
    });
 });
